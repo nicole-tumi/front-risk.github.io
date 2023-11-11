f05535d8cf8412dd3583af389bbfe39c666f5d17
Nicole Tumi <tuminicole@hotmail.com>
2023-11-11 11:13:32 -0500

    microfrontends update

--- mf-register/src/App.vue
+++ mf-register/src/App.vue
@@ -1,25 +1,43 @@
 <template>
-  <HelloWorld />
+    <HelloWorld />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+    name: "App",
+    components: {
+        HelloWorld,
+    },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+.input-section {
+    margin-bottom: 10px;
+}
+
+input {
+    height: 20px;
+    width: 90%;
+    padding: 10px;
+    margin-bottom: 10px;
+    border-radius: 5px;
+}
+button {
+    width: 150px;
+    padding: 10px;
+    border-radius: 10px;
+    font-size: medium;
+    cursor: pointer;
 }
 </style>
